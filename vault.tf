dc61f5559d2992c531046b6a61eb347826f51883
Connor O'Kane <connor.o'kane@justice.gov.uk>
2025-09-29 11:40:00 +0100

    fixing formatting and dependency issue

--- vault.tf
+++ vault.tf
@@ -1,9 +1,9 @@
 
 resource "vault_generic_secret" "redis_cache_access_keys" {
-  for_each  = var.redis_server_settings
+  for_each   = var.redis_server_settings
   depends_on = [azurerm_redis_cache.main]
-  path      = format("secret/terraform/%s/rediscache/%s", var.environment, each.key)
-  data_json = <<EOT
+  path       = format("secret/terraform/%s/rediscache/%s", var.environment, each.key)
+  data_json  = <<EOT
 {
   "primary_access_key": "${lookup(lookup(azurerm_redis_cache.main, each.key), "primary_access_key")}",
   "secondary_access_key": "${lookup(lookup(azurerm_redis_cache.main, each.key), "secondary_access_key")}"
