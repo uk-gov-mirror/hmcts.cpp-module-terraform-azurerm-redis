eb87a866a98bae25860279b30866b14b32ad2433
Narinder Doal <nsd17@hotmail.co.uk>
2026-01-14 17:05:04 +0000

    amenddeprecated argument

--- example/variables.tf
+++ example/variables.tf
@@ -19,7 +19,7 @@ variable "redis_server_settings" {
     sku_name                      = string
     enable_non_ssl_port           = optional(bool)
     minimum_tls_version           = optional(string)
-    public_network_access_enabled = optional(string)
+    public_network_access_enabled = optional(bool)
   }))
   description = "optional redis server setttings for both Premium and Standard/Basic SKU"
   default     = {}
@@ -92,4 +92,34 @@ variable "version_number" {
   type        = string
   description = "The version of the application or object being deployed. This could be a build object or other artefact which is appended by a CI/Cd platform as part of a process of standing up an environment"
   default     = ""
+}
+
+variable "subnet_id" {
+  description = "The ID of the Subnet for Premium SKU VNET integration. Not used for Basic/Standard SKUs with Private Endpoints."
+  type        = string
+  default     = null
+}
+
+variable "enable_private_endpoint" {
+  description = "Enable Private Endpoint for Basic/Standard SKU. Set to false for Premium SKU to use VNET integration instead."
+  type        = bool
+  default     = false
+}
+
+variable "private_endpoint_subnet_id" {
+  description = "The ID of the Subnet for Private Endpoint. Required when enable_private_endpoint is true for Basic/Standard SKUs."
+  type        = string
+  default     = null
+}
+
+variable "virtual_network_name" {
+  description = "The name of the Virtual Network for Private DNS zone linking. Required when enable_private_endpoint is true."
+  type        = string
+  default     = null
+}
+
+variable "virtual_network_resource_group_name" {
+  description = "The resource group name of the Virtual Network. Required when enable_private_endpoint is true."
+  type        = string
+  default     = null
 }
